82ca8070317caa65bda820c4d5f8c1b0c3cda163
JiHoon <kjhun9062@g.yju.ac.kr>
2023-10-16 21:26:01 +0900

    -Refactor: header component화 -Design: Header와 login-page 간단한 틀

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import { useNavigate } from "react-router-dom";
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  let navigate = useNavigate();
   return (
-    <div className="font-mono">
-      <div className="flex">
-        <div className="flex-1 m-5 text-5xl">Diary</div>
-        <div>
-          <button
-            class="middle none center m-3 rounded-lg bg-blue-500 py-2 px-4 font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            data-ripple-light="true"
-            onClick={() => {
-              navigate(""); //login페이지로 가기
-            }}
-          >
-            login
-          </button>
-        </div>
-      </div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
