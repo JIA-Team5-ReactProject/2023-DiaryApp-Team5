b7eb111703db7f1bdb1b3a2ed0a37862b3f01e7a
JiHoon <kjhun9062@g.yju.ac.kr>
2023-10-17 21:08:40 +0900

    Feat: DiaryCard 컴포넌트 구현, 백에서 파일 받아오기 전에 하드코딩

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,24 @@ function Header() {
     <div>
       <div className="flex m-5">
         <div className="flex-none text-5xl tracking-tighter font-mono mr-14">
-          Diary
+          <div className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-10 h-10"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+              />
+            </svg>
+
+            <span>Diary</span>
+          </div>
         </div>
         <div className="flex-1 flex tracking-tighter font-extralight">
           <div className="flex-none mr-10 place-self-end hover:cursor-pointer">
