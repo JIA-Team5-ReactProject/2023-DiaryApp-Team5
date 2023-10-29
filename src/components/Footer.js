d88b0facec7896b11f03ff621cefda9db3ecc834
gardenkim <oyasumiee2@gmail.com>
2023-10-29 23:15:55 +0900

    Design: Add Footer Component

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer className="font-extralight bg-white">
+      <div className="mx-auto max-w-screen-xl px-4 pb-8 pt-16 sm:px-6 lg:px-8">
+        <div className="mt-16 border-t border-gray-100 pt-8">
+          <p className="text-center text-xs/relaxed text-gray-500">
+            © Company 2022. All rights reserved.
+            <br />
+            Created with React.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
