de7c0594776dd1e26391087d52cb94344370349e
gardenkim <oyasumiee2@gmail.com>
2023-10-29 23:16:58 +0900

    Design: Change Tailwind properties

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,9 +6,9 @@ function Login() {
   };
 
   return (
-    <div className="mt-10 mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+    <div className="font-extralight mt-10 mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-lg text-center">
-        <h1 className="text-2xl font-bold sm:text-3xl">로그인</h1>
+        <h1 className="text-2xl sm:text-3xl">로그인</h1>
 
         <p className="mt-4 text-gray-500">
           로그인하고 나만의 일기를 작성하는 여정을 시작하세요.
