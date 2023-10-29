d649d111f1a76b2289988f12fcf189cdb74f7d7a
gardenkim <oyasumiee2@gmail.com>
2023-10-29 23:18:04 +0900

    Remove: Deleted the regex file as it was unnecessary

--- src/util/nicknameRegex.js
+++
@@ -1,6 +0,0 @@
-
-// 닉네임: 2자 ~ 10자 특수문자 제외
-
-const nicknameRegex = /^[\w\Wㄱ-ㅎㅏ-ㅣ가-힣]{2,20}$/;
-
-export default nicknameRegex;
--- src/util/passwordRegex.js
+++
@@ -1,6 +0,0 @@
-
-// 비밀번호: 하나 이상의 문자와 숫자 : 6 ~ 12자
-
-const passwordRegex = /^[A-Za-z0-9]{6,12}$/;
-
-export default passwordRegex;
