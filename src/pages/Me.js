c980d7c69e090fa25372981017d0cc6d5a07f062
Hyn2 <justin010129@gmail.com>
2023-10-30 20:42:28 +0900

    feat : Implement search logic

--- src/pages/Me.js
+++ src/pages/Me.js
@@ -1,14 +1,30 @@
 import DiaryCard from "../components/DiaryCard";
+import SearchBar from '../components/searchbar/SearchBar';
+import { useEffect, useState } from 'react';
+import getPost from '../util/getPost'
 
 function Me() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getPost().then((result) => {
+      setData(result);
+    }).catch((err) => {
+      console.error(err);
+    });
+  },[]);
+
+  console.log(data);
+
   return (
-    <div>
+    <>
+      <SearchBar />
       <div className="mt-4 grid grid-cols-3 gap-5 p-10">
-        <DiaryCard />
-        <DiaryCard />
-        <DiaryCard />
-      </div>
-    </div>
+        {data.map((element) => (
+          <DiaryCard key={element.id} name={element.user_id} />
+        ))}
+      </div>      
+    </>
+    
   );
 }
 
