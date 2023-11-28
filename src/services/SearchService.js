a8487ea577d5127640229808f5c23374a9ae04a7
Hyn2 <justin010129@gmail.com>
2023-11-28 19:15:05 +0900

    Fix: fix search logic

--- src/services/SearchService.js
+++ src/services/SearchService.js
@@ -1,16 +1,35 @@
 class SearchService {
-  static async getData(userId, searchBy, page) {
-    if (searchBy === "user" && userId !== "") {
+ 
+  static async getDataAll() {
       const request = await fetch(
-        `http://localhost:3001/diary?user_id=${userId}`
-      );
+        `http://localhost:3001/diary`);
       return request.json();
-    } else {
-      const request = await fetch(
-        `http://localhost:3001/diary?_start=0&_end=${page}`
-      );
-      return request.json();
-    }
+  }
+
+ /**
+   * 
+   * @param {int} userId user's id field
+   * @returns {array} json array
+   */
+  static async getDataByUser(userId) {
+    const request = await fetch(
+      `http://localhost:3001/diary?user_id=${userId}`);
+    return request.json();
+  }
+ /**
+   * 
+   * @param {array} data array data
+   * @param {String} keyWord search Keyword 
+   * @returns {array} json array
+   */
+  static filterByTitle(data, keyWord) {
+    const arr = [];
+    const lowerCaseKeyWord = keyWord.toLowerCase();
+    data.forEach((element) => {
+      const trimmedString = element.title.replace(/\s+/g, ''); // //사이에 정규표현식, \s+ 하나 이상의 공백을 ''로 대체
+      if (trimmedString.includes(lowerCaseKeyWord)) arr.push(element);
+    });
+    return arr;
   }
 }
 
